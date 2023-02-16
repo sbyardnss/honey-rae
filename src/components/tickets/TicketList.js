367d6250ab7ff02299f766704a14c30f436a1fcb
Mary Williams <sbyardnss@gmail.com>
2023-02-16 15:27:39 -0600

    ticketList styled

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,16 +1,36 @@
 import { useEffect, useState } from "react"
+import "./Tickets.css"
 // export const TicketList = () => {
 //     return <h2>List of Tickets</h2>
 // }
-
+    
 export const TicketList = () => {
     const [tickets, setTickets] = useState([])
 
     useEffect(
         () => {
-            console.log("Initial state of tickets", tickets) // View the initial state of tickets
+            fetch(`http://localhost:8088/serviceTickets`)
+                .then(response => response.json())
+                .then((ticketArray) => {
+                    setTickets(ticketArray)
+
+                })
         },
-        [] // When this array is empty, you are observing initial component state
+        []
     )
-    return <h2>List of Tickets</h2>
+    return <>
+        <h2>List of Tickets</h2>
+        <article className="tickets">
+            {
+                tickets.map(
+                    (ticket) => {
+                        return <section className="ticket">
+                            <header>{ticket.description}</header>
+                            <footer>Emergency: {ticket.emergency ? "bomb" : "No"}</footer>
+                        </section>
+                    }
+                )
+            }
+            </article>
+        </>
 }
